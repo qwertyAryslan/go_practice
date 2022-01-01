3fec4f5a63dd148bc2efc3adca8ec926a0d97896
qwertyAryslan <46024355+qwertyAryslan@users.noreply.github.com>
2022-01-01 18:32:31 +0600

    pratice 1

---
+++ hello_world.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	var string_output string = "Hello world!"
+	fmt.Println(string_output)
+}
---
+++ work_with_strings.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var (
+		name    = "Aryslan"
+		surname = "Bulantayev"
+	)
+
+	fmt.Println(name)
+	fmt.Println(surname)
+}
